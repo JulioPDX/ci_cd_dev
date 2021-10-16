6dcc3b82c1048a58dc01fafccd34162b19a625b3
JulioPDX <juliop503@gmail.com>
2021-10-16 09:34:53 -0700

    Testing connection to devices

--- hello.py
+++ hello.py
@@ -1,4 +1,21 @@
 import os
 from rich import print
 
-print("10.10.10.10/24")
+from netmiko import ConnectHandler
+
+cisco1 = {
+    "device_type": "cisco_ios",
+    "host": "192.168.10.122",
+    "username": "admin",
+    "password": os.getenv("MY_PASS"),
+}
+
+# Show command that we execute
+command = "show ip int brief"
+with ConnectHandler(**cisco1) as net_connect:
+    output = net_connect.send_command(command)
+
+# Automatically cleans-up the output so that only the show output is returned
+print()
+print(output)
+print()
