a28a1a484a1312823973288fdc43599b49b1bb44
JulioPDX <juliop503@gmail.com>
2021-10-17 11:52:55 -0700

    Testing exec pipelines and napalm

--- hello.py
+++ hello.py
@@ -1,22 +1,56 @@
 """Simple script used for testing deployments"""
 import os
+import re
+import napalm
+import pandas as pd
 from rich import print as rprint
+from pybatfish.client.commands import *
+from pybatfish.datamodel import *
+from pybatfish.datamodel.answer import *
+from pybatfish.datamodel.flow import *
+from pybatfish.question import *
+from pybatfish.question import bfq
 
-from netmiko import ConnectHandler
 
-cisco1 = {
-    "device_type": "cisco_ios",
-    "host": "192.168.10.122",
-    "username": "admin",
-    "password": os.getenv("MY_PASS"),
-}
+PASS = os.getenv("MY_PASS")
 
-# Show command that we execute
-COMMAND = "show ip int brief"
-with ConnectHandler(**cisco1) as net_connect:
-    output = net_connect.send_command(COMMAND)
+devices = [
+    {
+        "hostname": "pdx-rtr-vios-01",
+        "device_type": "ios",
+        "host": "192.168.10.122",
+        "username": "admin",
+        "password": PASS,
+    },
+    {
+        "hostname": "pdx-rtr-veos-01",
+        "device_type": "eos",
+        "host": "192.168.10.151",
+        "username": "admin",
+        "password": PASS,
+    },
+]
 
-# Automatically cleans-up the output so that only the show output is returned
-print()
-rprint(output)
-print()
+
+for device in devices:
+    driver = napalm.get_network_driver(device["device_type"])
+    temp_device = driver(
+        hostname=device["host"],
+        username=device["username"],
+        password=device["password"],
+    )
+    temp_device.open()
+    config = temp_device.get_config(retrieve="running")
+    run_conf = config["running"]
+
+    # erase lines with "Building configuration", "Current Configuration" and "end"
+    run_config = re.sub(
+        r"Building configuration.*|Current configuration.*|end", "", run_conf
+    )
+
+    ### create file with running config in backup_config folder
+    with open(
+        f"./snapshots/configs/{device['hostname']}.txt", "w", encoding="utf-8"
+    ) as file:
+        file.write(run_config)
+    temp_device.close()
