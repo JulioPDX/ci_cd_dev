a5e001697ad1483e4a9476726885949a27cf847b
JulioPDX <juliop503@gmail.com>
2021-10-17 20:42:48 -0700

    adding spacing

--- bat.py
+++ bat.py
@@ -9,9 +9,11 @@ from pybatfish.question import *
 from pybatfish.question import bfq
 
 bf_session.host = "192.168.10.184"
+
 NETWORK_NAME = "PDX_NET"
 SNAPSHOT_NAME = "snapshot00"
 SNAPSHOT_DIR = "./configs_compare"
+
 bf_set_network(NETWORK_NAME)
 bf_init_snapshot(SNAPSHOT_DIR, name=SNAPSHOT_NAME, overwrite=True)
 load_questions()
