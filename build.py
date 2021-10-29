f58c57b9d7952d39050fc350375c9c0c7816c040
juliopdx <juliop503@gmail.com>
2021-10-29 09:50:54 -0700

    added argparse for dev vs prod napalm check

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 """Script used to configure the network"""
+import argparse
 from rich import print as rprint
 from rich import inspect
 from nornir import InitNornir
@@ -7,13 +8,21 @@ from nornir_utils.plugins.functions import print_result
 from tools import nornir_set_creds
 
 
+parser = argparse.ArgumentParser()
+# Returns True if argument is set.
+# If missing then will run on production.
+parser.add_argument("--dry_run", type=bool, help="To be or not to be")
+args = parser.parse_args()
+print(args.dry_run)
+
+
 def deploy_network(task):
     """Configures network with NAPALM and retrives backup for comparison"""
     task1_result = task.run(
         name=f"Configuring {task.host.name}!",
         task=napalm_configure,
         filename=f"./snapshots/configs/{task.host.name}.txt",
-        dry_run=True,
+        dry_run=args.dry_run,
         replace=True,
     )
 
