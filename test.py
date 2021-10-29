239fddf360b1e377272803b5c1f2f974d71036e2
juliopdx <juliop503@gmail.com>
2021-10-29 15:40:31 -0700

    rewriting test structure

--- test.py
+++ test.py
@@ -3,32 +3,59 @@
 
 from pybatfish.client.commands import *
 from pybatfish.question import load_questions
-from pybatfish.question import bfq
+from pybatfish.client.asserts import (
+    assert_no_duplicate_router_ids,
+    assert_no_incompatible_bgp_sessions,
+    assert_no_incompatible_ospf_sessions,
+    assert_no_unestablished_bgp_sessions,
+    assert_no_undefined_references,
+)
 from rich import print as rprint
 
 
-def test_bgp_status():
-    """Checking BGP status"""
-    rprint(bfq.bgpSessionStatus().answer().frame())
-    assert (
-        bfq.bgpSessionStatus()
-        .answer()
-        .frame()
-        .query("Established_Status != 'ESTABLISHED'")
-        .empty
+def test_duplicate_rtr_ids(snap):
+    """Testing for duplicate router IDs"""
+    rprint("[bold yellow]Testing for duplicate router IDs")
+    assert_no_duplicate_router_ids(
+        snapshot=snap,
+        protocols={"ospf", "bgp"},
     )
+    rprint("[bold green]No duplicate router IDs found")
 
 
-def test_ospf_status():
-    """Checking OSPF status"""
-    rprint(bfq.ospfSessionCompatibility().answer().frame())
-    assert (
-        bfq.ospfSessionCompatibility()
-        .answer()
-        .frame()
-        .query("Session_Status != 'ESTABLISHED'")
-        .empty
+def test_bgp_compatibility(snap):
+    """Testing for incompatible BGP sessions"""
+    rprint("[bold yellow]Testing for incompatible BGP sessions")
+    assert_no_incompatible_bgp_sessions(
+        snapshot=snap,
     )
+    rprint("[bold green]All BGP sessions compatible!")
+
+
+def test_ospf_compatibility(snap):
+    """Testing for incompatible OSPF sessions"""
+    rprint("[bold yellow]Testing for incompatible OSPF sessions")
+    assert_no_incompatible_ospf_sessions(
+        snapshot=snap,
+    )
+    rprint("[bold green]All OSPF sessions compatible!")
+
+
+def test_bgp_unestablished(snap):
+    """Testing for BGP sessions that are not established"""
+    rprint("[bold yellow]Testing for unestablished BGP sessions")
+    assert_no_unestablished_bgp_sessions(
+        snapshot=snap,
+    )
+    rprint("[bold green]All BGP sessions are established!")
+
+def test_undefined_references(snap):
+    """Testing for any undefined references"""
+    rprint("[bold yellow]Testing for undefined references")
+    assert_no_undefined_references(
+        snapshot=snap,
+    )
+    rprint("[bold green]No undefined refences found!")
 
 
 def init_bf():
@@ -39,8 +66,13 @@ def init_bf():
 
     bf_session.host = "192.168.10.184"
     bf_set_network(NETWORK_NAME)
-    bf_init_snapshot(SNAPSHOT_DIR, name=SNAPSHOT_NAME, overwrite=True)
+    init_snap = bf_init_snapshot(SNAPSHOT_DIR, name=SNAPSHOT_NAME, overwrite=True)
     load_questions()
+    test_duplicate_rtr_ids(init_snap)
+    test_bgp_compatibility(init_snap)
+    test_ospf_compatibility(init_snap)
+    test_bgp_unestablished(init_snap)
+    test_undefined_references(init_snap)
 
 
 init_bf()
