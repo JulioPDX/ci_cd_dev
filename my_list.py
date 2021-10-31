41d8a1d64b860399c25d56b2a67213a5bfaf9c44
juliopdx <juliop503@gmail.com>
2021-10-31 09:42:03 -0700

    fixing file format

--- my_list.py
+++ my_list.py
@@ -1,1 +1,8 @@
-my_list = ["something1", "something2", "something3", "something4", "something5", "something6"]
+my_list = [
+    "something1",
+    "something2",
+    "something3",
+    "something4",
+    "something5",
+    "something6",
+]
