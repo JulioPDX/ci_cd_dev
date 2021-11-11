50e111ade4be18dfb40bfcc3408f31b1306f8b5b
juliopdx <juliop503@gmail.com>
2021-11-11 13:47:39 -0800

    Sticking with emojis for UI

--- test_suzieq.py
+++ test_suzieq.py
@@ -12,7 +12,9 @@ ospf_df = pd.DataFrame(ospf_tbl().aver())
 ospf_fail = 0
 for index, row in ospf_df.iterrows():
     if row["assert"] != "pass":
-        print(f"[bold yellow]OSPF, {row['assertReason']}[/bold yellow]")
+        print(
+            f":triangular_flag_on_post: OSPF, {row['assertReason']} :triangular_flag_on_post:"
+        )
         ospf_fail += 1
 
 # BGP testing
@@ -22,13 +24,13 @@ bgp_fail = 0
 for index, row in bgp_df.iterrows():
     if row["state"] != "Established":
         print(
-            f"{row['hostname']}(AS {row['asn']}) to {row['peer']}(AS {row['peerAsn']}) is in {row['state']} state"
+            f":triangular_flag_on_post: {row['hostname']}(AS {row['asn']}) to {row['peer']}(AS {row['peerAsn']}) is in {row['state']} state :triangular_flag_on_post:"
         )
         bgp_fail += 1
 
 if bgp_fail == 0:
-    print("All BGP checks passed")
+    print(":white_heavy_check_mark: All BGP checks passed :white_heavy_check_mark:")
 if ospf_fail == 0:
-    print("All OSPF checks passed")
+    print(":white_heavy_check_mark: All OSPF checks passed :white_heavy_check_mark:")
 if bgp_fail or ospf_fail != 0:
     sys.exit(1)
