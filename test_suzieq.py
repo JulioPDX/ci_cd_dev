2cd3011529a86d2a077accbf40d7f18dba463ccc
juliopdx <juliop503@gmail.com>
2021-11-11 13:22:50 -0800

    testing out different failure options and printing

--- test_suzieq.py
+++ test_suzieq.py
@@ -9,26 +9,29 @@ from rich import print, inspect
 # OSPF Testing
 ospf_tbl = get_sqobject("ospf")
 ospf_df = pd.DataFrame(ospf_tbl().get())
-for row_label, row in ospf_df.iterrows():
+ospf_fail = 0
+for index, row in ospf_df.iterrows():
     if row["adjState"] != "passive":
         if row["adjState"] != "full":
-            print(ospf_tbl().aver())
-            print(ospf_df.to_string())
-            sys.exit(1)
-        else:
-            print(f"{row['hostname']} to {row['peerHostname']} is in state: {row['adjState']}")
+            print(
+                f"{row['hostname']} to area {row['area']} peer is in {row['adjState']} state"
+            )
+            ospf_fail += 1
 
 # BGP testing
 bgp_tbl = get_sqobject("bgp")
 bgp_df = pd.DataFrame(bgp_tbl().get())
 bgp_fail = 0
-for row_label, row in bgp_df.iterrows():
+for index, row in bgp_df.iterrows():
     if row["state"] != "Established":
         print(
             f"{row['hostname']}(AS {row['asn']}) to {row['peer']}(AS {row['peerAsn']}) is in {row['state']} state"
         )
         bgp_fail += 1
-if bgp_fail != 0:
-    sys.exit(1)
-else:
+
+if bgp_fail == 0:
     print("All BGP checks passed")
+if ospf_fail == 0:
+    print("All OSPF checks passed")
+if bgp_fail or ospf_fail != 0:
+    sys.exit(1)
